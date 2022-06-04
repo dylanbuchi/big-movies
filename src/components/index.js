cf18f78c8a809b288f7fdcbee86ba7763a436c93
Dylan Buchi <devybuchi@gmail.com>
2022-06-04 10:35:13 -0300

    Add index.js files

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Actors } from './Actors/Actors';
+export { default as MovieInfo } from './MovieInfo/MovieInfo';
+export { default as Movies } from './Movies/Movies';
+export { default as NavBar } from './NavBar/NavBar';
+export { default as Profile } from './Profile/Profile';
