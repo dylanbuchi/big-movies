c509c1db76a25c6894401ef3a5abc4a218a92293
Dylan Buchi <devybuchi@gmail.com>
2022-06-09 10:07:30 -0300

    Add ThemeProvider to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({});
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
 );
