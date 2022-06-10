85ecd71640f2e28d6faa335d2021b56782f5c41f
Dylan Buchi <devybuchi@gmail.com>
2022-06-10 20:05:06 -0300

    Add SideBar to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as MovieInfo } from './MovieInfo/MovieInfo';
 export { default as Movies } from './Movies/Movies';
 export { default as NavBar } from './NavBar/NavBar';
 export { default as Profile } from './Profile/Profile';
+export { default as SideBar } from './SideBar/SideBar';
