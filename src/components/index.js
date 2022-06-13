f43889cb5d0ff3a4c8d7880d3fe3e05f2a0618aa
Dylan Buchi <devybuchi@gmail.com>
2022-06-13 15:34:55 -0300

    Add Movie and MovieList to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { default as Movies } from './Movies/Movies';
 export { default as NavBar } from './NavBar/NavBar';
 export { default as Profile } from './Profile/Profile';
 export { default as SideBar } from './SideBar/SideBar';
+export { default as MovieList } from './MovieList/MovieList';
+export { default as Movie } from './Movie/Movie';
