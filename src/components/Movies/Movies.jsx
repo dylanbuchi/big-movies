f3c67bb25a707a348cf236df79b58ee19870958f
Dylan Buchi <devybuchi@gmail.com>
2022-06-13 16:27:01 -0300

    Add movie data to Movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,36 @@
+import { Box, CircularProgress, Typography } from '@mui/material';
+
+import { useGetPopularMoviesQuery } from '../../services/the_movie_database_api';
+import MovieList from '../MovieList/MovieList';
+
 const Movies = () => {
-  console.log('');
-  return <div>Movies</div>;
+  const { data, isError, isFetching } = useGetPopularMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">No movies found</Typography>
+      </Box>
+    );
+  }
+
+  if (isError) {
+    return <Box>Error!</Box>;
+  }
+
+  return (
+    <Box>
+      <MovieList movies={data} />
+    </Box>
+  );
 };
 
 export default Movies;
