71481ddb65fde9ad038e05e51d4099fe437e61f0
Dylan Buchi <devybuchi@gmail.com>
2022-06-13 11:20:34 -0300

    Add the movie database api

---
+++ src/services/the_movie_database_api.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const theMovieDatabaseApiKey = process.env.REACT_APP_THE_MOVIE_DATABASE_API_KEY;
+
+const theMovieDatabaseApiBaseUrl = 'https://api.themoviedb.org/3/';
+const page = 1;
+
+export const theMovieDatabaseApi = createApi({
+  reducerPath: 'theMovieDatabaseApi',
+
+  baseQuery: fetchBaseQuery({ baseUrl: theMovieDatabaseApiBaseUrl }),
+  endpoints: (builder) => ({
+    getPopularMovies: builder.query({
+      query: () =>
+        `movie/popular?api_key=${theMovieDatabaseApiKey}&page=${page}`,
+    }),
+  }),
+});
+
+export const { useGetPopularMoviesQuery } = theMovieDatabaseApi;
