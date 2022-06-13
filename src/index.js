7f283e661151ba2319baea2347a33dd0259a0f8e
Dylan Buchi <devybuchi@gmail.com>
2022-06-13 11:17:41 -0300

    Add Redux to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 const theme = createTheme({});
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
 );
