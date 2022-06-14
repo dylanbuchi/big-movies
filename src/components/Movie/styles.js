dc306658031df13037a628b6625e9ff6e7489166
Dylan Buchi <devybuchi@gmail.com>
2022-06-14 18:27:44 -0300

    Add Movie component styles

---
+++ src/components/Movie/styles.js
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+
+import { styled } from '@mui/material/styles';
+
+export const StyledLink = styled(Link)(() => ({
+  alignItems: 'center',
+  fontWeight: 'bolder',
+  textDecoration: 'none',
+  overflow: 'hidden',
+
+  '&:hover': {
+    cursor: 'pointer',
+    textShadow: '0.1px 0.1px',
+    filter: 'brightness(1.05)',
+  },
+}));
+
+export const MoviePoster = styled('img')(() => ({
+  width: '208px',
+  height: '300px',
+  marginBottom: '0',
+  borderRadius: '15px',
+
+  transition: 'all 0.5s',
+  '&:hover': {
+    transform: 'scale(1.08)',
+  },
+}));
+
+export const typographyStyle = {
+  color: 'black',
+  textOverflow: 'ellipsis',
+  alignItems: 'center',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+
+  marginTop: '5px',
+  marginBottom: '50px',
+  wordBreakMode: 'break',
+};
