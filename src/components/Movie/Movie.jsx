7152dbf6ec767a40f0fd799cd5684aa5ec74d567
Dylan Buchi <devybuchi@gmail.com>
2022-06-14 18:27:23 -0300

    Add movie titles, images and update styles

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,32 +1,46 @@
-import { Typography, Grid } from '@mui/material';
+import { Typography, Grid, Grow } from '@mui/material';
+import { StyledLink, MoviePoster, typographyStyle } from './styles';
 
-export const Movie = ({ movie }) => (
-  <Grid
-    item
-    xs={12}
-    sm={6}
-    md={4}
-    lg={3}
-    xl={2}
-    padding="20px"
-    sx={{ ml: { xs: '60px', sm: 0 } }}
-  >
-    <Typography
-      variant="h5"
+export const Movie = ({ movie, index }) => {
+  const movieImageUrl = 'https://image.tmdb.org/t/p/w500/';
+  const noImageUrL =
+    'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
+
+  const checkMovieTitleLength = (movieTitle, maxLength) =>
+    movieTitle.length < maxLength
+      ? movieTitle
+      : `${movieTitle.split('').splice(0, 18).join('')}...`;
+
+  return (
+    <Grid
+      item
+      xs={12}
+      sm={6}
+      md={4}
+      lg={3}
+      xl={2}
       sx={{
-        color: 'primary',
-        textOverflow: 'ellipsis',
-        textAlign: 'center',
-        whiteSpace: 'nowrap',
-        overflow: 'hidden',
-        width: '190px',
-        marginTop: '10px',
-        marginBottom: '0',
+        ml: { xs: '20%', sm: 0 },
       }}
     >
-      {movie.title}
-    </Typography>
-  </Grid>
-);
+      <Grow in key={index} timeout={(index + 1) * 225}>
+        <StyledLink to={`/movies/${movie.id}`}>
+          <MoviePoster
+            src={
+              movie.poster_path
+                ? `${movieImageUrl}${movie.poster_path}`
+                : `${noImageUrL}`
+            }
+            alt={movie.title}
+          />
+
+          <Typography variant="h6" sx={typographyStyle}>
+            {checkMovieTitleLength(movie.title, 22)}
+          </Typography>
+        </StyledLink>
+      </Grow>
+    </Grid>
+  );
+};
 
 export default Movie;
