7e6888200c2e09fde3dc4d4695ed84aa994ec6bc
Dylan Buchi <devybuchi@gmail.com>
2022-06-15 15:47:33 -0300

    Add Grid style to MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,7 +3,12 @@ import { Grid } from '@mui/material';
 import { Movie } from '../Movie/Movie';
 
 const MovieList = ({ movies }) => (
-  <Grid container spacing={0}>
+  <Grid
+    marginLeft={{ sm: '5px', md: '-40px', lg: '-60px' }}
+    container
+    spacing={{ xs: 1, md: 7, lg: 2 }}
+    justifyContent={{ xs: 'center', sm: 'left' }}
+  >
     {movies.results.map((movie, i) => (
       <Movie key={i} index={i} movie={movie} />
     ))}
