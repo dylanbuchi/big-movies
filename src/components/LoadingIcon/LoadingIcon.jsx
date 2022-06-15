653f61966d0aafbc28eb03f1903087f95f9da7b8
Dylan Buchi <devybuchi@gmail.com>
2022-06-15 16:20:35 -0300

    Add LoadingIcon component

---
+++ src/components/LoadingIcon/LoadingIcon.jsx
@@ -0,0 +1,9 @@
+import { Box, CircularProgress } from '@mui/material';
+
+const LoadingIcon = () => (
+  <Box display="flex" justifyContent="center">
+    <CircularProgress size="4rem" />
+  </Box>
+);
+
+export default LoadingIcon;
