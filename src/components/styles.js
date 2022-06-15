67e36ea4feeff5820b831b2cebfedf8f79a5a770
Dylan Buchi <devybuchi@gmail.com>
2022-06-15 15:46:35 -0300

    Fix padding in styles.js

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,4 +1,4 @@
-import { styled } from '@mui/system';
+import { styled } from '@mui/material/styles';
 
 export const DivRoot = styled('div')(() => ({
   display: 'flex',
@@ -10,7 +10,14 @@ export const DivToolbar = styled('div')(() => ({
   height: '70px',
 }));
 
-export const MainContent = styled('main')(() => ({
+export const MainContent = styled('main')(({ theme }) => ({
   flexGrow: '1',
-  padding: '2em',
+  paddingTop: '2.5em',
+
+  [theme.breakpoints.up('lg')]: {
+    paddingLeft: '22em',
+  },
+
+  [theme.breakpoints.only('sm')]: { paddingLeft: '14.5em' },
+  [theme.breakpoints.only('md')]: { paddingLeft: '17em' },
 }));
