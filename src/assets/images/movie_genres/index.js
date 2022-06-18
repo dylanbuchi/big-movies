5d0583cd53394b082d9186d2a9cbb514207e85f8
Dylan Buchi <devybuchi@gmail.com>
2022-06-18 19:48:15 -0300

    Add movie genre icon images and index.js

---
+++ src/assets/images/movie_genres/index.js
@@ -0,0 +1,59 @@
+import action from './action.png';
+import adventure from './adventure.png';
+import animation from './animation.png';
+
+import comedy from './comedy.png';
+import crime from './crime.png';
+
+import documentary from './documentary.png';
+import drama from './drama.png';
+
+import family from './family.png';
+import fantasy from './fantasy.png';
+
+import history from './history.png';
+import horror from './horror.png';
+
+import music from './music.png';
+import mystery from './mystery.png';
+
+import romance from './romance.png';
+import scienceFiction from './science-fiction.png';
+
+import tvMovie from './tv-movie.png';
+import thriller from './thriller.png';
+
+import war from './war.png';
+import western from './western.png';
+
+const movieGenreIcons = {
+  action,
+  adventure,
+  animation,
+
+  comedy,
+  crime,
+
+  documentary,
+  drama,
+
+  family,
+  fantasy,
+
+  history,
+  horror,
+
+  music,
+  mystery,
+
+  romance,
+  'science fiction': scienceFiction,
+
+  'tv movie': tvMovie,
+  thriller,
+
+  war,
+  western,
+};
+
+export default movieGenreIcons;
