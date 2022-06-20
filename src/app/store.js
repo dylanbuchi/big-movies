2362dcbe7a23444dec7c14bb2d2f741edaf5ec14
Dylan Buchi <devybuchi@gmail.com>
2022-06-20 01:14:56 -0300

    Add `movieCategoryOrGenreReducer` to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { theMovieDatabaseApi } from '../services/the_movie_database_api';
 
+import movieCategoryOrGenreReducer from '../features/movie_category_or_genre';
+
 export default configureStore({
   reducer: {
     [theMovieDatabaseApi.reducerPath]: theMovieDatabaseApi.reducer,
+    movieCategoryOrGenre: movieCategoryOrGenreReducer,
   },
 });
