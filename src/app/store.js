ec712daa888e1207dbd09610dea87c90adea29f8
Dylan Buchi <devybuchi@gmail.com>
2022-06-20 07:27:31 -0300

    Add searchMovie to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { theMovieDatabaseApi } from '../services/the_movie_database_api';
 
-import movieCategoryOrGenreReducer from '../features/movie_category_or_genre';
+import movieCategoryOrGenreSlice_ from '../features/movie_category_or_genre';
+import searchMovieSlice_ from '../features/search_movie';
 
 export default configureStore({
   reducer: {
     [theMovieDatabaseApi.reducerPath]: theMovieDatabaseApi.reducer,
-    movieCategoryOrGenre: movieCategoryOrGenreReducer,
+    movieCategoryOrGenre: movieCategoryOrGenreSlice_,
+    searchMovie: searchMovieSlice_,
   },
 });
