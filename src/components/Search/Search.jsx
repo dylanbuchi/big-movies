0a49d18fb9f1dfe4bae796ed903257799bd3f64d
Dylan Buchi <devybuchi@gmail.com>
2022-06-21 03:01:05 -0300

    Add a clear button in Search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,17 +2,45 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Search as SearchIcon } from '@mui/icons-material';
-import { TextField, InputAdornment, Box } from '@mui/material';
+import { TextField, InputAdornment, Box, Button } from '@mui/material';
 import { searchMovie } from '../../features/search_movie';
 
 const Search = () => {
   const [query, setQuery] = useState('');
+
   const dispatch = useDispatch();
 
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
       dispatch(searchMovie(query));
     }
+  };
+
+  const clearTextFieldInput = () => {
+    setQuery('');
+  };
+
+  const displayClearButton = () => {
+    if (query) {
+      return (
+        <Button
+          onClick={() => {
+            clearTextFieldInput();
+            dispatch(searchMovie(''));
+          }}
+          variant="contained"
+          size="small"
+          sx={{
+            borderRadius: '15px',
+            textTransform: 'none',
+            m: '2.5px',
+          }}
+        >
+          Clear
+        </Button>
+      );
+    }
+    return '';
   };
 
   return (
@@ -40,6 +68,7 @@ const Search = () => {
               <SearchIcon />
             </InputAdornment>
           ),
+          endAdornment: displayClearButton(),
           disableUnderline: true,
           sx: (theme) => ({
             color: theme.palette.mode === 'light' ? 'black' : 'white',
