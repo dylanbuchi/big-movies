3113619fee93c70257510613855c6717a8151500
Dylan Buchi <devybuchi@gmail.com>
2022-06-22 05:14:04 -0300

    Add user authentication redux slice

---
+++ src/features/user_authentication.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userAuthenticationSlice = createSlice({
+  name: 'userAuthentication',
+  initialState: {
+    isAuthenticated: false,
+    sessionId: '',
+    user: {},
+  },
+  reducers: {
+    setUser: (state, action) => {
+      state.isAuthenticated = true;
+      state.sessionId = localStorage.getItem('session_id');
+      state.user = action.payload;
+
+      localStorage.setItem('userAccountId', action.payload.id);
+    },
+  },
+});
+
+export const { setUser } = userAuthenticationSlice.actions;
+
+export default userAuthenticationSlice.reducer;
