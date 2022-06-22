892f96fea1575c3951128b9bdd16d5a3d88b52e7
Dylan Buchi <devybuchi@gmail.com>
2022-06-22 05:12:30 -0300

    Add user authentication to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import { theMovieDatabaseApi } from '../services/the_movie_database_api';
 
 import movieCategoryOrGenreSlice_ from '../features/movie_category_or_genre';
 import searchMovieSlice_ from '../features/search_movie';
+import authenticationSlice_ from '../features/authentication';
 
 export default configureStore({
   reducer: {
     [theMovieDatabaseApi.reducerPath]: theMovieDatabaseApi.reducer,
     movieCategoryOrGenre: movieCategoryOrGenreSlice_,
     searchMovie: searchMovieSlice_,
+    userAuthentication: authenticationSlice_,
   },
 });
