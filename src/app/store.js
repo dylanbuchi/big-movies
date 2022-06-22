cf6f9eb1688e48b40ff9a7917b5e482257c57b16
Dylan Buchi <devybuchi@gmail.com>
2022-06-22 05:16:27 -0300

    Update import name

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,7 @@ import { theMovieDatabaseApi } from '../services/the_movie_database_api';
 
 import movieCategoryOrGenreSlice_ from '../features/movie_category_or_genre';
 import searchMovieSlice_ from '../features/search_movie';
-import authenticationSlice_ from '../features/authentication';
+import authenticationSlice_ from '../features/user_authentication';
 
 export default configureStore({
   reducer: {
