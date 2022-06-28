100c641091b4a0c921246fb50cfe3ebf6c03306c
Dylan Buchi <devybuchi@gmail.com>
2022-06-28 13:19:23 -0300

    Add length property to MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,14 +2,14 @@ import { Grid } from '@mui/material';
 
 import { Movie } from '..';
 
-const MovieList = ({ movies }) => (
+const MovieList = ({ movies, length }) => (
   <Grid
     marginLeft={{ sm: '5px', md: '-40px', lg: '-60px' }}
     container
     spacing={{ xs: 1, md: 7, lg: 2 }}
     justifyContent={{ xs: 'center', sm: 'left' }}
   >
-    {movies.results.map((movie, i) => (
+    {movies.results.slice(0, length).map((movie, i) => (
       <Movie key={i} index={i} movie={movie} />
     ))}
   </Grid>
