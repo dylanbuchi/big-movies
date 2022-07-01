a00da455a4676513a319d9feb9ea612344fe6c4c
Dylan Buchi <devybuchi@gmail.com>
2022-07-01 10:09:11 -0300

    Add DarkModeProvider

--- src/index.js
+++ src/index.js
@@ -3,21 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
 import App from './components/App';
 import store from './app/store';
+import DarkModeProvider from './utilities/ToggleDarkMode';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const theme = createTheme({});
-
 root.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
+    <DarkModeProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>
+    </DarkModeProvider>
   </Provider>,
 );
