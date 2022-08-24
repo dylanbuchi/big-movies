80f93626366ffa2565d4bcd31ff9b36847019a00
Dylan Buchi <devybuchi@gmail.com>
2022-08-24 03:08:52 -0300

    Add searchInputField state and setInputField reducer

--- src/features/search_movie.js
+++ src/features/search_movie.js
@@ -4,14 +4,18 @@ export const searchMovieSlice = createSlice({
   name: 'searchMovie',
   initialState: {
     searchMovie: '',
+    searchInputField: '',
   },
   reducers: {
     searchMovie: (state, action) => {
       state.searchMovie = action.payload;
     },
+    setInputField: (state, action) => {
+      state.searchInputField = action.payload;
+    },
   },
 });
 
-export const { searchMovie } = searchMovieSlice.actions;
+export const { searchMovie, setInputField } = searchMovieSlice.actions;
 
 export default searchMovieSlice.reducer;
